7fe92a287f4fe1d4bd051d30512b1109de3fa744
Subramanya Chakravarthy <chanty.chakravarthy@gmail.com>
2023-09-27 23:14:51 +0530

    sidebar generated

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,6 +15,18 @@ export default defineConfig({
     sidebar: [{
       label: 'Introduction',
       autogenerate: { directory: '1-introduction' },
+    }, {
+      label: 'Usage',
+      autogenerate: {directory: '2-usage'}
+    }, {
+      label: 'React',
+      autogenerate: {directory: '3-react'}
+    }, {
+      label: 'Guides',
+      autogenerate: {directory: '4-guides'}
+    }, {
+      label: 'Other',
+      autogenerate: {directory: '5-other'}
     }]
   }), react(), tailwind({applyBaseStyles: false})],
   // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
