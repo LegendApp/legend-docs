2f0fd90101f880693ebc5a4eb8efeec060f41eda
Subramanya Chakravarthy <chanty.chakravarthy@gmail.com>
2023-09-28 23:16:47 +0530

    Intro section is completed

---
+++ src/Components/Introduction/MemoArrayExample.tsx
@@ -0,0 +1,45 @@
+import { useRef } from "react";
+import { Editor } from "../Editor/Editor";
+import { Memo, useObservable } from "@legendapp/state/react";
+import { useInterval } from "usehooks-ts";
+
+const MEMO_ARRAY_EXAMPLE_CODE = `
+  function MemoArrayExample() {
+    const renderCount = ++useRef(0).current;
+    const messages = useObservable([]);
+
+    useInterval(() => {
+        messages.splice(0, 0, \`Message \${messages.length + 1}\`);
+    }, 1000);
+
+    return (
+        <div className="p-4 text-md bg-gray-800" style={{ width: 150 }}>
+            <div>Renders: {renderCount}</div>
+            <Memo>
+                {() => <div className="pt-4 h-[300px] overflow-auto">
+                    {messages.map((m, i) => (
+                        <div key={i}>{m}</div>
+                    ))}
+                </div>}
+            </Memo>
+        </div>
+    );
+  }
+
+  render(<MemoArrayExample />)
+`;
+
+export function MemoArrayExampleComponent() {
+  Memo;
+  return (
+    <Editor
+      code={MEMO_ARRAY_EXAMPLE_CODE}
+      scope={{
+        useRef,
+        useObservable,
+        Memo,
+        useInterval,
+      }}
+    />
+  );
+}
---
+++ src/Components/Introduction/MemoExample.tsx
@@ -0,0 +1,66 @@
+import React, { useRef, useState } from "react";
+import { Editor } from "../Editor/Editor";
+import {
+  Memo,
+  enableLegendStateReact,
+  observer,
+  useObservable,
+} from "@legendapp/state/react";
+import { useInterval } from "usehooks-ts";
+import { observable } from "@legendapp/state";
+import { FlashingDiv } from "../FlashingDiv/FlashingDiv";
+
+const MEMO_CODE = `
+  const MemoExample = () => {
+    const renderCount = ++useRef(0).current;
+    const state$ = useObservable({ count: 0 });
+    const [value, setValue] = useState(1);
+
+    useInterval(() => {
+      state$.count.set((v) => v + 1);
+    }, 500);
+
+    const onClick = () => setValue((v) => v + 1);
+
+    return (
+      <div className="p-4 text-md bg-gray-800" style={{ width: "150px" }}>
+        <div>Renders: {Math.max(renderCount, 1)}</div>
+        <div>Value: {value}</div>
+        <button
+          className="block px-4 py-2 my-8 font-bold bg-gray-700 rounded shadow text-2xs hover:bg-gray-600 active:bg-gray-500"
+          onClick={onClick}
+        >
+          Render parent
+        </button>
+        <Memo>
+          {() => <>
+            <div>Value: {value}</div>
+            <div className="pt-4">Count: {state$.count.get()}</div>
+          </>}
+        </Memo>
+      </div>
+    );
+  };
+
+  render(<MemoExample />)
+`;
+
+export function MemoExampleComponent() {
+  return (
+    <Editor
+      code={MEMO_CODE}
+      scope={{
+        useRef,
+        useObservable,
+        Memo,
+        observable,
+        enableLegendStateReact,
+        useInterval,
+        observer,
+        React,
+        FlashingDiv,
+        useState,
+      }}
+    />
+  );
+}
