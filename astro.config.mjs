8ffea8c1fa7e4839c0d48d4c500f4aa26285f87b
Subramanya Chakravarthy <chanty.chakravarthy@gmail.com>
2023-10-03 22:07:49 +0530

    usage group is done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,38 +1,48 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-    title: 'Legend State',
-    customCss: ['./src/tailwind.css'],
-    social: {
-      github: 'https://github.com/LegendApp/legend-state'
-    },
-    sidebar: [{
-      label: 'Introduction',
-      autogenerate: { directory: '1-introduction' },
-    }, {
-      label: 'Usage',
-      autogenerate: {directory: '2-usage'}
-    }, {
-      label: 'React',
-      autogenerate: {directory: '3-react'}
-    }, {
-      label: 'Guides',
-      autogenerate: {directory: '4-guides'}
-    }, {
-      label: 'Other',
-      autogenerate: {directory: '5-other'}
-    }]
-  }), react(), tailwind({applyBaseStyles: false})],
+  integrations: [
+    starlight({
+      title: "Legend State",
+      customCss: ["./src/tailwind.css"],
+      social: {
+        github: "https://github.com/LegendApp/legend-state",
+      },
+      sidebar: [
+        {
+          label: "Introduction",
+          autogenerate: { directory: "1-introduction" },
+        },
+        {
+          label: "Usage",
+          autogenerate: { directory: "2-usage" },
+        },
+        {
+          label: "React",
+          autogenerate: { directory: "3-react" },
+        },
+        {
+          label: "Guides",
+          autogenerate: { directory: "4-guides" },
+        },
+        {
+          label: "Other",
+          autogenerate: { directory: "5-other" },
+        },
+      ],
+    }),
+    react(),
+    tailwind({ applyBaseStyles: false }),
+  ],
   // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
   image: {
     service: {
-      entrypoint: 'astro/assets/services/sharp'
-    }
-  }
+      entrypoint: "astro/assets/services/sharp",
+    },
+  },
 });
