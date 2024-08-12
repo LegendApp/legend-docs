98a751b24415a43f9c6f9cefb886338d7346d220
Jay Meistrich <jmeistrich@gmail.com>
2024-08-12 11:38:51 -0700

    Add prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/** @type {import("prettier").Config} */
+const config = {
+    tabWidth: 4,
+    printWidth: 120,
+    singleQuote: true,
+};
+
+module.exports = config;
