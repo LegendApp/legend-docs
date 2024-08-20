d36bdadccd53d15473ec85faa41bc7b5b996f45e
Jay Meistrich <jmeistrich@gmail.com>
2024-08-20 11:23:49 -0700

    Fix math in preorder and tweak opacity in animation

--- packages/state/src/Components/Home/Preorder.tsx
+++ packages/state/src/Components/Home/Preorder.tsx
@@ -4,6 +4,10 @@ import { Text } from './Text';
 import { PreorderButton } from './PreorderButton';
 
 export const Preorder = observer(function Preorder() {
+    const FullPrice = 399
+    const DiscountedPrice = 199;
+    const Discount = FullPrice - DiscountedPrice;
+    const DiscountPercentage = (Discount / FullPrice) * 100;
     return (
         <div className="sm:flex items-center gap-8 mt-section px-4">
             <div className="flex-1">
@@ -21,12 +25,12 @@ export const Preorder = observer(function Preorder() {
                 <Header size="h2">Legend Kit</Header>
                 <Text>
                     Get lifetime access to everything in Legend Kit for a single one-time purchase, including all future
-                    updates. Preorder now to save $100 and accelerate Legend Kit's development.
+                    updates. Preorder now to save ${Discount} and accelerate Legend Kit's development.
                 </Text>
-                <div className="text-4xl !mt-8 line-through opacity-60">$399</div>
-                <div className="text-5xl !mt-2">$199</div>
+                <div className="text-4xl !mt-8 line-through opacity-60">${FullPrice}</div>
+                <div className="text-5xl !mt-2">${DiscountedPrice}</div>
                 <Text className="!mt-2">
-                    Save $200 (50% off) with preorder!
+                    Save ${Discount} ({DiscountPercentage.toFixed(0)}% off) with preorder!
                 </Text>
                 <PreorderButton color="white" className="!mt-8">
                     Preorder Now
