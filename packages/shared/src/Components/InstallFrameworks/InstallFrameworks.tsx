daf439597be00b2297fa37d86f5c51673bf09325
Jay Meistrich <jmeistrich@gmail.com>
2024-09-16 16:45:03 +0200

    Update for v3 beta

--- packages/shared/src/Components/InstallFrameworks/InstallFrameworks.tsx
+++ packages/shared/src/Components/InstallFrameworks/InstallFrameworks.tsx
@@ -1,27 +1,23 @@
-import { observer } from "@legendapp/state/react";
-import classNames from "classnames";
-import { state$ } from "../../state";
-import { InstallCode, InstallTabs } from "../Install/Install";
+import { observer } from '@legendapp/state/react';
+import classNames from 'classnames';
+import { state$ } from '../../state';
+import { InstallCode, InstallTabs } from '../Install/Install';
 
-export const InstallFrameworks = observer(function ({
-  className,
-}: {
-  className?: string;
-}) {
-  const name = "@legendapp/state@alpha";
-  const framework = state$.framework.get();
-  return (
-    <div className={classNames("mt-6", className)}>
-      <InstallTabs name={name} />
-      <InstallCode name={name} />
-      {framework === "React Native" && (
-        <div>
-          <div>And install your preferred storage plugin</div>
-          <InstallCode name="react-native-mmkv" />
-          <div className="!mt-2 -mb-2">or</div>
-          <InstallCode name="@react-native-async-storage/async-storage" />
+export const InstallFrameworks = observer(function ({ className }: { className?: string }) {
+    const name = '@legendapp/state@beta';
+    const framework = state$.framework.get();
+    return (
+        <div className={classNames('mt-6', className)}>
+            <InstallTabs name={name} />
+            <InstallCode name={name} />
+            {framework === 'React Native' && (
+                <div>
+                    <div>And install your preferred storage plugin</div>
+                    <InstallCode name="react-native-mmkv" />
+                    <div className="!mt-2 -mb-2">or</div>
+                    <InstallCode name="@react-native-async-storage/async-storage" />
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 });
