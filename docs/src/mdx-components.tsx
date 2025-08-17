5c2d4eee066fd5bfd87eaac3c0a5057ed365aa58
Jay Meistrich <jmeistrich@gmail.com>
2025-08-17 20:39:55 +0100

    Add live editing

--- docs/src/mdx-components.tsx
+++ docs/src/mdx-components.tsx
@@ -1,10 +1,12 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
+import { Editor } from '@/components/Editor';
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    Editor,
     ...components,
   };
 }
