82f8cf2cbab54c6d576f6442b2cc21be39a3342d
Jay Meistrich <jmeistrich@gmail.com>
2025-09-12 17:35:07 +0200

    Fix formatting of headers on state home

--- docs/src/components/state/home/Header.tsx
+++ docs/src/components/state/home/Header.tsx
@@ -1,33 +1,39 @@
-import { createElement, type ReactNode } from "react";
-import classNames from "classnames";
+import { createElement, type ReactNode } from 'react';
+import classNames from 'classnames';
 
 interface Props {
-  children: ReactNode;
-  size: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
-  className?: string;
-  fontWeight?: string;
-  leading?: string;
-  color?: `${'!' | ''}text-${string}`;
+    children: ReactNode;
+    size: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+    className?: string;
+    fontWeight?: string;
+    leading?: string;
+    color?: `${'!' | ''}text-${string}`;
 }
 
-export function Header({
-  children,
-  color,
-  size,
-  className,
-  leading,
-  fontWeight,
-}: Props) {
-  return createElement(
-    size,
-    {
-      className: classNames(
-        color || "text-white",
-        leading || "!leading-normal",
-        fontWeight || "font-bold",
-        className,
-      ),
-    },
-    children
-  );
+export function Header({ children, color, size, className, leading, fontWeight }: Props) {
+    return createElement(
+        size,
+        {
+            className: classNames(
+                color || 'text-white',
+                leading || '!leading-normal',
+                fontWeight || 'font-bold',
+                size === 'h1'
+                    ? 'text-4xl sm:text-5xl'
+                    : size === 'h2'
+                      ? 'text-3xl sm:text-4xl'
+                      : size === 'h3'
+                        ? 'text-2xl sm:text-3xl'
+                        : size === 'h4'
+                          ? 'text-xl sm:text-2xl'
+                          : size === 'h5'
+                            ? 'text-lg sm:text-xl'
+                            : size === 'h6'
+                              ? 'text-base sm:text-lg'
+                              : '',
+                className,
+            ),
+        },
+        children,
+    );
 }
