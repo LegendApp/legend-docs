55644841050f6844529f9d981b5ef7d029c1e1e7
Jay Meistrich <jmeistrich@gmail.com>
2025-10-13 15:43:58 +0200

    add google tag manager to docs

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import '@/app/global.css';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
+import { GoogleTagManager } from '@next/third-parties/google';
+
 import { Provider } from './provider';
 
 const inter = Inter({
@@ -9,9 +11,14 @@ const inter = Inter({
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
+    const gtmId = process.env.GTM_ID || '';
+    if (!gtmId) {
+        console.error('NEXT_PUBLIC_GTM_ID is not set');
+    }
     return (
         <html lang="en" className={inter.className} suppressHydrationWarning>
             <body className="flex flex-col min-h-screen">
+                {gtmId && <GoogleTagManager gtmId={gtmId} />}
                 <Provider>{children}</Provider>
             </body>
         </html>
