89e5118d7aa0514420ab82f7b9ba3177e8401160
Jay Meistrich <jmeistrich@gmail.com>
2025-10-14 23:32:57 +0200

    remove the old packages/

--- packages/shared/config.ts
+++
@@ -1,6 +0,0 @@
-export const versions = [
-  ["v3", "v3 (next)"],
-  ["v2", "v2"],
-];
-
-export const defaultVersion = "v3";
--- packages/state/src/Components/Home/SectionKitDevTools.tsx
+++
@@ -1,8 +0,0 @@
-export function SectionKitDevTools() {
-  return (
-    <div className="flex">
-      <h3>Dev Tools</h3>
-      <p className="text-gray-400">More details coming soon...</p>
-    </div>
-  );
-}
